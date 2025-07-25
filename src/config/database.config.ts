e3222125ac450809785226926a8f235f73cb1df3
Farzin Farooque <farsinfarooque916@gmail.com>
2025-07-25 20:05:17 +0530

    added non-null assertion

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 const dialect = process.env.DB_DIALECT as Dialect;
 
-export const sequelize = new Sequelize(process.env.DB_URL, {
+export const sequelize = new Sequelize(process.env.DB_URL!, {
   dialect: "postgres",
   dialectOptions: {
     ssl: {
