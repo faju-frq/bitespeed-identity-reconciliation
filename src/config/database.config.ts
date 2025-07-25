f18b92dd80b2d96a96741e281d3b63dd0b76aa18
Farzin Farooque <farsinfarooque916@gmail.com>
2025-07-25 19:54:07 +0530

    chore(render): create branch for Render hosting and update Sequelize config for Render PostgreSQL

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,13 +3,12 @@ import "dotenv/config";
 
 const dialect = process.env.DB_DIALECT as Dialect;
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD!,
-  {
-    host: process.env.DB_HOST,
-    dialect: dialect,
-    logging: false,
-  }
-);
+export const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorised: false,
+    },
+  },
+});
